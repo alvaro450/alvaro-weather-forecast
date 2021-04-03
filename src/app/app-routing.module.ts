ade2d07b0d70c1969e2f6eefcb8db718759ee1f8
alvaro450 <alvaro450@gmail.com>
2021-04-03 10:19:29 -0400

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { DashboardComponent } from "./dashboard/dashboard.component";
 
 // Both of the available paths are lazy loaded
 const ROUTES: Routes = [
